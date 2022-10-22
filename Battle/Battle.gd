31c37f9c90863a0abb40bc2cd88a3ce8c17da661
Timothy Foster <tf.auroratide@gmail.com>
2022-10-22 14:55:26 -0500

    Enemies collection for dynamic search

--- Battle/Battle.gd
+++ Battle/Battle.gd
@@ -2,9 +2,22 @@ extends Node
 
 class_name Battle
 
+
+
 func _ready():
-	$Slime/AttackOption.assign($BlueberryPike, $Slime)
+	$Enemies/Slime/AttackOption.assign($BlueberryPike, $Enemies/Slime)
 
 
 func _on_AttackOption_attack_finished():
-	$Slime.attack(self)
+	$Enemies/Slime.attack(self)
+
+func show_attack_options():
+	# for each enemy on the scene
+	# for each weapon that can actually hit
+	# Show the attack options
+	# Assign the option accordingly
+	pass
+
+func hide_attack_options():
+	# I guess just destroy them
+	pass
