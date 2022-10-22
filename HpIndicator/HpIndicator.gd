198c8ddfa2cf0a0a885af56632e267b0d8ca7d9f
ChromeWisp <chromewisp@gmail.com>
2022-10-22 15:36:09 -0500

    Empty hearts are represented as crumbs

--- HpIndicator/HpIndicator.gd
+++ HpIndicator/HpIndicator.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
-export(Texture) var texture
+export(Texture) var fullTexture
+export(Texture) var emptyTexture
 
 func showHp(curHp: int, maxHp: int):
 	for n in $Hearts.get_children():
@@ -10,7 +11,13 @@ func showHp(curHp: int, maxHp: int):
 	for n in range(curHp):
 		var sprite = Sprite.new()
 		sprite.position.x = n * 100
-		sprite.texture = texture
+		sprite.texture = fullTexture
+		$Hearts.add_child(sprite)
+		
+	for n in range(maxHp - curHp):
+		var sprite = Sprite.new()
+		sprite.position.x = (n + curHp) * 100
+		sprite.texture = emptyTexture
 		$Hearts.add_child(sprite)
 
 func _on_health_changed(curHp, maxHp):
